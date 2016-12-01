d199df4de1c37c0faa1b8dd8f3bb4fe89e77a211
CutitaruAdrian <cutitaru.adryan@gmail.com>
2016-12-01 02:52:43 +0200

    Publish Task2

---
+++ app/src/main/java/com/example/cutit_000/lebogitsearch/Main2Activity.java
@@ -0,0 +1,138 @@
+package com.example.cutit_000.lebogitsearch;
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private String TAG = Main2Activity.class.getSimpleName();
+    private ListView lv;
+    ArrayList<HashMap<String, String>> RepositoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        RepositoryList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetRepositories().execute();
+
+    }
+
+    private class GetRepositories extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(Main2Activity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String url = ("https://api.github.com/users/"+ MainActivity.MyClass.user+"/repos");
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+
+                try {
+                    JSONArray repositories = new JSONArray(jsonStr);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < repositories.length(); i++) {
+                        JSONObject r = repositories.getJSONObject(i);
+                        String name = r.getString("name");
+                        //afisare in consola (test pentru nume)
+                        System.out.println(name);
+
+                        // tmp hash map for single repository
+                        HashMap<String, String> repository = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        repository.put("name", name);
+
+                        // adding repository to repository list
+                        RepositoryList.add(repository);
+
+
+
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(Main2Activity.this, RepositoryList, R.layout.list_item, new String[]{"name"},
+                    new int[]{R.id.name});
+            lv.setAdapter(adapter);
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent newActivity = new Intent(Main2Activity.this, Rep_Details.class);
+                    newActivity.putExtra("position", RepositoryList.get(position));
+                    startActivity(newActivity);}
+            });
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/cutit_000/lebogitsearch/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.cutit_000.lebogitsearch;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.os.StrictMode;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static class MyClass{
+
+        public static String  user;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        Button button = (Button) findViewById(R.id.button);
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setEnabled(false);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                HttpsURLConnection connection;
+                TextView textView = (TextView) findViewById(R.id.textView);
+                EditText editText = (EditText) findViewById(R.id.editText);
+                MyClass.user = editText.getText().toString();
+
+
+                try {
+                    URL url = new URL("https://github.com/"+MyClass.user);
+                    connection = (HttpsURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+
+                    int code = connection.getResponseCode();
+
+                    if (code == 200) {
+                        textView.setText("User existent");
+                        button2.setEnabled(true);
+
+                    }
+
+
+                    else {
+                        textView.setText("!!! User inexistent !!!");
+                        button2.setEnabled(false);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    button2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        } });
+
+    }
+
+
+
+
+}
